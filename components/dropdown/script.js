0f9d6ae92b2de2dd71486d064a39dbe73f8136e4
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-19 19:06:14 +1000

    added search function

--- components/dropdown/script.js
+++ components/dropdown/script.js
@@ -1,3 +1,9 @@
 function dropdown() {
     document.getElementById('dropdown').classList.toggle('dropdown');
 }
+
+function onSearch() {
+    const input = document.getElementById('search');
+    const val = input.value;
+    console.log(val);
+}
