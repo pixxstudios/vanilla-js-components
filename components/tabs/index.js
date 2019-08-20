3c5f133c03d60d33eaec0e28b2924382cf70694c
Gagandeep Singh <gagan.ddun@gmail.com>
2019-08-20 18:46:25 +1000

    code update

--- components/tabs/index.js
+++ components/tabs/index.js
@@ -1,8 +1,18 @@
 window.addEventListener('load', function() {
     // get all li items
     var tabs = document.querySelectorAll('ul.nav-tabs > li');
-    console.log(tabs);
 
+    // tabs click handler
+    function tabsClick(e) {
+        // remove active class from all the tabs
+        for(var i=0; i<tabs.length; i++) {
+            tabs[i].classList.remove('active');
+        }
+        e.currentTarget.classList.add('active');
+        e.preventDefault();
+    }
+
+    // add click event on all tabs
     for(var i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener('click', tabsClick);
     }
